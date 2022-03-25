3e826ae5a643dd8df21e82ef10df0c25c8dd48aa
SatyaWork <satyanarayana.e@allvy.com>
2022-03-25 11:33:48 +0530

    changes

--- src/main/java/com/sbms/SpringBootDockerTestApplication.java
+++ src/main/java/com/sbms/SpringBootDockerTestApplication.java
@@ -2,8 +2,10 @@ package com.sbms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootDockerTestApplication {
 
 	public static void main(String[] args) {
